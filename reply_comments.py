80d2bd8c636ee5d026a772553c25f756451488a8
Elif <35169772+elifkoseler@users.noreply.github.com>
2018-12-27 19:30:43 +0300

    Update reply_comments.py

--- reply_comments.py
+++ reply_comments.py
@@ -1,14 +1,35 @@
+from hashlib import sha256
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+
+def true_password(hsh2):
+  if(hsh1 == hsh2):
+    return 1
+  else:
+    return 0
+
+pw1 = input('Please enter your password:')
+hsh1 = create_hash(pw1)
+
 mylist = []
-
 def take_input():
-  myvalue = input("Enter your comment: \n")
-  mylist.append(myvalue)
-  
-  print("Previously entered comments:")
-  for comment in mylist:
-    i = mylist.index(comment)
+  what = true_password(hsh2)
+  if(what):
+    mylist.append(myvalue)
+    print("Previously entered comments:")
+    for comment in mylist:
+      i = mylist.index(comment)
     
-    print(i+1,"-",comment)
-
+      print(i+1,"-",comment)
+  else:
+    print("I am sorry I can't let you do that.")
+    
 while(1):
+  myvalue = input("Enter your comment: \n")
+  pw2 = input('Enter your password:\n')
+  hsh2 = create_hash(pw2)
   take_input()
+  
+  
